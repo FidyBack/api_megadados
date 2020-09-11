010039202109344e85b67ae4ce8d7ef4f0f886ba
Rodrigo <38670550+Rodrigo2104@users.noreply.github.com>
2020-09-11 20:30:28 -0300

    aps part 1

--- main.py
+++ main.py
@@ -5,22 +5,64 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-
-class Item(BaseModel):
+class Food(BaseModel):
     name: str
-    price: float
-    is_offer: Optional[bool] = None
+    quantity: int
+    bought: bool
 
+database={
+    1:Food(name= "pasta", quantity=50, bought=1),
+    2:Food(name= "pineaple", quantity=100, bought=0)
+}
 
-@app.get("/")
+@app.get("/items")
 def read_root():
-    return {"Hello": "World"}
-
+    
+    return database
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(item_id: int):
+    if (item_id in database):
+        return database[item_id]
+    else:
+        pass
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/items/bought/{paid}")
+def read_bought_item(paid: bool):
+    data={}
+    for i in database:
+        if (database[i].bought==paid):
+            data[i]=database[i]
+            return data
+    
+@app.post("/items/add/{item_id}")
+def update_item(item_id: int, item: Food):
+    if not(item_id in database):
+        database[item_id]= item
+    else:
+        pass
+
+@app.put("/items/update/{item_id}")
+def update_item(item_id: int, amount: int):
+    if (item_id in database):
+        database[item_id].quantity = amount
+    else:
+        pass
+
+@app.put("items/bought/{item_id}")
+def bought_item(item_id: int, paid: bool):
+    if (item_id in database):
+        database[item_id].bought = paid
+    else:
+        pass
+
+@app.delete("/items/delete/{item_id}")
+def delete_item(item_id:int):
+    if (item_id in database):
+        del database[item_id]
+        return ("deletion ")
+    else:
+        pass
+
+
+
